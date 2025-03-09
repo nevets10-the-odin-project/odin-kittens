4d93ad2379767052ecff3161df2ac1213d1d1a97
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-03-08 18:27:20 -0800

    Add seed data, update layout of kittens/index

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+[
+  { name: 'Athena', age: 10, cuteness: 'very', softness: 'very' },
+  { name: 'Bob', age: 1, cuteness: 'much', softness: 'none' },
+  { name: 'Garfield', age: 14, cuteness: 'meh', softness: 'a little' },
+  { name: 'Moe', age: 10, cuteness: 'ok', softness: 'much' },
+  { name: 'Snowflake', age: 1, cuteness: 'extremely', softness: 'lots' },
+  { name: 'Star', age: 3, cuteness: 'peak', softness: 'yes' },
+  { name: 'Grumpy', age: 7, cuteness: 'hilarious', softness: 'the most' },
+  { name: 'Don', age: 5, cuteness: 'not', softness: 'nope' },
+  { name: 'Cheese', age: 2, cuteness: 'kinda', softness: 'yes?' },
+  { name: 'Darwin', age: 4, cuteness: 'very', softness: 'acceptable' },
+  { name: 'Pancake', age: 3, cuteness: 'yes', softness: 'yup' },
+  { name: 'Poe', age: 8, cuteness: 'probably', softness: 'I think so?' }
+].each do |kitten|
+  Kitten.find_or_create_by!(name: kitten[:name], age: kitten[:age], cuteness: kitten[:cuteness],
+                            softness: kitten[:softness])
+end
