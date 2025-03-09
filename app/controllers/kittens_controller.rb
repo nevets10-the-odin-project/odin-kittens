95c0fb84c93b9f9b2534f66cf2eb835135cd1b98
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-03-08 18:12:28 -0800

    Populate kitten controller methods with base vars

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,22 +1,35 @@
 class KittensController < ApplicationController
   def index
+    @kittens = Kitten.all
   end
 
   def show
+    @kitten = Kitten.find(params[:id])
   end
 
   def new
+    @kitten = Kitten.new
   end
 
   def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      redirect_to @kitten
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+    @kitten = Kitten.find(params[:id])
   end
 
   def update
+    @kitten = Kitten.find(params[:id])
   end
 
   def destroy
+    @kitten = Kitten.find(params[:id])
   end
 end
